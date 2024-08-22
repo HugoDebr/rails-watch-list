7496c286c110b6c95aa17613de420a501841879a
hugo debrabandere <hugo@pitchable.fr>
2024-08-22 04:48:18 +0200

    Prepare rails app with external specs

---
+++ app/models/movie.rb
@@ -0,0 +1,10 @@
+class Movie < ApplicationRecord
+  has_many :bookmarks
+
+  validates :title, presence: true
+  validates :title, uniqueness: true
+  validates :overview, presence: true
+  validates :overview, uniqueness: true
+  validates :poster_url, presence: true
+  validates :rating, presence: true
+end
